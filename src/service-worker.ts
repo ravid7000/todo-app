36510ed96f8195fbb94d1cef1daee46f28b724d0
Ravi Dhiman <ravid7000@gmail.com>
2021-05-04 14:16:25 +0530

    server worker registration updated

--- src/service-worker.ts
+++
@@ -1,7 +0,0 @@
-export function register() {
-  window.addEventListener('load', () => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js')
-    }
-  })
-}
