a0b587030c25e083a762d8735e2b33f344a961e4
Ravi Dhiman <ravid7000@gmail.com>
2021-05-04 15:13:56 +0530

    gitignored

---
+++ serviceWorker/index.js
@@ -0,0 +1,17 @@
+/// <reference lib="webworker" />
+
+import { createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching';
+import { registerRoute, NavigationRoute } from 'workbox-routing';
+import { skipWaiting, clientsClaim } from 'workbox-core';
+
+skipWaiting();
+clientsClaim();
+
+const precacheManifest = [].concat(self.__WB_MANIFEST || []);
+precacheAndRoute(precacheManifest);
+
+const handler = createHandlerBoundToURL('/index.html');
+const navigationRoute = new NavigationRoute(handler, {
+  denylist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
+});
+registerRoute(navigationRoute);
